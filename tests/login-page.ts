7426423d382ac41d24719aeee324dd0e6df78d96
kundzios <kundzios@gmail.com>
2021-11-08 04:10:01 +0100

    Finished login tests

--- tests/login-page.ts
+++ tests/login-page.ts
@@ -3,8 +3,6 @@ import { expect, Locator, Page } from '@playwright/test';
 export class LoginPage {
 	readonly page: Page;
 	readonly loginButton: Locator;
-	readonly forgotPasswordLink: Locator;
-	readonly registerLink: Locator;
 	readonly emailInput: Locator;
 	readonly passwordInput: Locator;
 	readonly visibilityToggle: Locator;
@@ -12,23 +10,13 @@ export class LoginPage {
 	constructor(page: Page) {
 		this.page = page;
 		this.loginButton = page.locator('#log_in');
-		this.forgotPasswordLink = page.locator('text=Forgot Password?');
-		this.registerLink = page.locator('text=Register Now!');
-		this.emailInput = page.locator('input #user_email');
-		this.passwordInput = page.locator('input #user_password');
+		this.emailInput = page.locator('input#user_email');
+		this.passwordInput = page.locator('input#user_password');
 		this.visibilityToggle = page.locator('.toggle-password');
 	}
 	
 	async goto() {
-		await this.page.goto('https://staging.paymi.com/');
-	}
-	
-	async forgotPassword() {
-		await this.forgotPasswordLink.click();
-	}
-	
-	async register() {
-		await this.registerLink.click();
+		await this.page.goto('https://staging.paymi.com/users/sign_in');
 	}
 	
 	async login() {
--- tests/login-test.spec.ts
+++ tests/login-test.spec.ts
@@ -1,24 +1,133 @@
 import { test, expect } from '@playwright/test';
 import { LoginPage } from './login-page.ts';
 
-const emailErrorId = '#email-error .error-text';
+/******************************************************************/
+/********* add valid credentials before starting the test *********/
+/***/ const credentialsEmail = '';
+/***/ const credentialsPassword = '';
+/******************************************************************/
+/******************************************************************/
+
+const urlHome = 'https://staging.paymi.com';
+const urlLogin = 'https://staging.paymi.com/users/sign_in';
+const urlDashboard = 'https://staging.paymi.com/dashboard/accounts';
+
+const passwordInputLocator = 'input#user_password';
+const emailErrorLocator = '#email-error .error-text';
+const passwordErrorLocator = '#password-error .error-text';
+const loginErrorLocator = '.error-box__errors';
+
 const emailErrorTextBlank = 'Email can\'t be blank';
 const emailErrorTextInvalid = 'Email is invalid';
-
-const passwordErrorId = '#password-error .error-text';
 const passwordErrorTextBlank = 'Password can\'t be blank';
-
-const loginErrorClass = '.error-box__errors';
 const loginErrorText = 'Invalid email address or password.';
 
-test('blank login', async ({ page }) => {
-	const loginPage = new LoginPage(page);
-	await loginPage.goto();
-	await loginPage.login();
-	const emailError = page.locator(emailErrorId);
-	const passwordError = page.locator(passwordErrorId);
-	await expect(emailError).toBeVisible();
-	await expect(emailError).toHaveText(emailErrorTextBlank);
-	await expect(passwordError).toBeVisible();
-	await expect(passwordError).toHaveText(passwordErrorTextBlank);
+test('redirect before login', async ({ page }) => {
+	await page.goto(urlHome);
+	await expect(page).toHaveURL(urlLogin);
+	expect(await page.screenshot()).toMatchSnapshot('login-page.png');
+});
+
+test.describe('login attempts', () => {
+	let page: Page;
+	let loginPage: LoginPage;
+	let emailError: Locator;
+	let passwordError: Locator;
+	
+	test.beforeAll(async ({ browser }) => {
+		page = await browser.newPage();
+		loginPage = new LoginPage(page);
+		await loginPage.goto();
+		emailError = page.locator(emailErrorLocator);
+		passwordError = page.locator(passwordErrorLocator);
+	});
+	
+	test.afterAll(async () => {
+		await page.close();
+	});
+	
+	test('blank login', async () => {
+		await loginPage.login();
+		await expect(emailError).toBeVisible();
+		await expect(emailError).toHaveText(emailErrorTextBlank);
+		await expect(passwordError).toBeVisible();
+		await expect(passwordError).toHaveText(passwordErrorTextBlank);
+	});
+	
+	test('invalid email without password', async () => {
+		await loginPage.inputEmail('abc');
+		await loginPage.inputPassword('');
+		await loginPage.login();
+		await expect(emailError).toBeVisible();
+		await expect(emailError).toHaveText(emailErrorTextInvalid);
+		await expect(passwordError).toBeVisible();
+		await expect(passwordError).toHaveText(passwordErrorTextBlank);
+	});
+	
+	test('blank email without password', async () => {
+		await loginPage.inputEmail('');
+		await loginPage.inputPassword('');
+		await loginPage.login();
+		await expect(emailError).toBeVisible();
+		await expect(emailError).toHaveText(emailErrorTextBlank);
+		await expect(passwordError).toBeVisible();
+		await expect(passwordError).toHaveText(passwordErrorTextBlank);
+	});
+	
+	test('blank email with password', async () => {
+		await loginPage.inputEmail('');
+		await loginPage.inputPassword('abc');
+		await loginPage.login();
+		await expect(emailError).toBeVisible();
+		await expect(emailError).toHaveText(emailErrorTextBlank);
+		await expect(passwordError).toBeHidden();
+	});
+	
+	test('invalid email with password', async () => {
+		await loginPage.inputEmail('abc');
+		await loginPage.inputPassword('abc');
+		await loginPage.login();
+		await expect(emailError).toBeVisible();
+		await expect(emailError).toHaveText(emailErrorTextInvalid);
+		await expect(passwordError).toBeHidden();
+	});
+	
+	test('valid email without password', async () => {
+		await loginPage.inputEmail('abc@abc.com');
+		await loginPage.inputPassword('');
+		await loginPage.login();
+		await expect(emailError).toBeHidden();
+		await expect(passwordError).toBeVisible();
+		await expect(passwordError).toHaveText(passwordErrorTextBlank);
+	});
+	
+	test('wrong credentials login', async () => {
+		let loginError = page.locator(loginErrorLocator);
+		await loginPage.inputEmail('abc@abc.com');
+		await loginPage.inputPassword('abc');
+		await loginPage.login();
+		await expect(loginError).toBeVisible();
+		await expect(loginError).toHaveText(loginErrorText);
+	});
+
+	test('password visibility check', async () => {
+		const passwordInput = page.locator(passwordInputLocator);
+		await loginPage.inputPassword('abc');
+		await loginPage.togglePasswordVisibility();
+		await expect(passwordInput).toHaveAttribute('type', 'text');
+		await loginPage.togglePasswordVisibility();
+		await expect(passwordInput).toHaveAttribute('type', 'password');
+	});
+	
+	test('successful login', async () => {
+		await loginPage.inputEmail(credentialsEmail);
+		await loginPage.inputPassword(credentialsPassword);
+		await loginPage.login();
+		await expect(page).toHaveURL(urlDashboard);
+	});
+	
+	test('redirect from login page', async () => {
+		await page.goto(urlLogin);
+		await expect(page).toHaveURL(urlDashboard);
+	});
 });
